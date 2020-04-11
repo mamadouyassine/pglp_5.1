3ef92baa98057a4d8020059ef171283716e31a69
uvsq21920982 <yassinemamadou97@gmail.com>
2020-04-11 03:19:49 +0200

    PersonnelDAO

---
+++ src/main/java/fr/uvsq/pglp/PersonnelsDAO.java
@@ -0,0 +1,27 @@
+package fr.uvsq.pglp;
+
+public class PersonnelsDAO extends DAO<Personnels> {
+    @Override
+    public Personnels create(Personnels obj) {
+        return obj;
+    }
+
+    @Override
+    public Personnels find(String rechercher) {
+
+        Personnels perso=null;
+        return perso;
+    }
+
+    @Override
+    public Personnels update(Personnels obj) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void delete(Personnels obj) {
+        // TODO Auto-generated method stub
+
+    }
+}
