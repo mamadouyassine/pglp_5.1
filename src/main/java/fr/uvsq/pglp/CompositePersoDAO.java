fc5af62aabb9709f6f50b30f6bc4ca6b91e39e91
uvsq21920982 <yassinemamadou97@gmail.com>
2020-04-11 03:19:09 +0200

    CompositeDAO

---
+++ src/main/java/fr/uvsq/pglp/CompositePersoDAO.java
@@ -0,0 +1,30 @@
+package fr.uvsq.pglp;
+
+public class CompositePersoDAO extends DAO<CompositePerso>{
+
+    @Override
+    public CompositePerso create(CompositePerso obj) {
+        // TODO Auto-generated method stub
+        return obj;
+    }
+
+    @Override
+    public CompositePerso find(String id) {
+        // TODO Auto-generated method stub
+        CompositePerso grpperso=null;
+        return grpperso;
+    }
+
+    @Override
+    public CompositePerso update(CompositePerso obj) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void delete(CompositePerso obj) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
